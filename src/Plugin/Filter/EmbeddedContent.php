9e1f742717a148e991ff716e4f853c8c0e80ff4d
nuez <teun@nuezweb.com>
2023-06-15 21:18:54 +0200

    3364980 Fix parsing of HTML

--- src/Plugin/Filter/EmbeddedContent.php
+++ src/Plugin/Filter/EmbeddedContent.php
@@ -6,6 +6,7 @@ use Drupal\Component\Serialization\Json;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Render\Renderer;
 use Drupal\Core\Security\TrustedCallbackInterface;
 use Drupal\ckeditor5_embedded_content\EmbeddedContentPluginManager;
@@ -73,37 +74,41 @@ class EmbeddedContent extends FilterBase implements ContainerFactoryPluginInterf
   public function process($text, $langcode) {
     $document = Html::load($text);
     $crawler = new Crawler($document);
-    $attachments = [];
+    $bubbleable = new BubbleableMetadata();
     $crawler->filter('embedded-content')
-      ->each(function (Crawler $crawler) use ($document, &$attachments) {
+      ->each(function (Crawler $crawler) use ($document, $bubbleable) {
         /** @var \DOMElement $node */
         $node = $crawler->getNode(0);
         $plugin_config = Json::decode($node->getAttribute('data-plugin-config'));
         $plugin_id = $node->getAttribute('data-plugin-id');
 
         if(!$plugin_id || !$plugin_config){
-
-        } else {
-          try {
-            /** @var \Drupal\ckeditor5_embedded_content\EmbeddedContentInterface $instance */
-            $instance = $this->embeddedContentPluginManager->createInstance($plugin_id, $plugin_config);
-            $attachments = BubbleableMetadata::mergeAttachments($attachments, $instance->getAttachments());
-            $replacement = $instance->build();
-            $render = $this->renderer->renderPlain($replacement);
-          } catch (Exception $e){
-            $render = (new TranslatableMarkup('Something went wrong.'));
-          }
-          // Don't throw html5 errors such as embedded media.
-          libxml_use_internal_errors(TRUE);
-          $new = Html::load($render);
-          libxml_clear_errors();
-          $new_node = $document->importNode($new->documentElement, TRUE);
-          libxml_use_internal_errors(FALSE);
-          $node->parentNode->replaceChild($new_node, $node);
+          return;
         }
+        try {
+          /** @var \Drupal\ckeditor5_embedded_content\EmbeddedContentInterface $instance */
+          $instance = $this->embeddedContentPluginManager->createInstance($plugin_id, $plugin_config);
+          $bubbleable->addAttachments($instance->getAttachments());
+          $replacement = $instance->build();
+          $context = new RenderContext();
+          $render = $this->renderer->executeInRenderContext($context, fn() => $this->renderer->render($replacement));
+          if (!$context->isEmpty()) {
+            $bubbleable->merge($context->pop());
+          }
+        } catch (Exception $e){
+          $render = (new TranslatableMarkup('Something went wrong.'));
+        }
+        // Don't throw html5 errors such as embedded media.
+        libxml_use_internal_errors(TRUE);
+        $new = Html::load($render);
+        libxml_clear_errors();
+        $xpath = new \DOMXPath($new);
+        $new_node = $document->importNode($xpath->query('//body')->item(0), TRUE);
+        libxml_use_internal_errors(FALSE);
+        $node->parentNode->replaceChild($new_node, $node);
       });
     $result = new FilterProcessResult(Html::serialize($document));
-    $result->addAttachments($attachments);
+    $result->merge($bubbleable);
     return $result;
   }
 
